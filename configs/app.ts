078376cb44472bc3a943f34c07756cd5345377b9
tenkuuninja <27910240+tenkuuninja@users.noreply.github.com>
2024-07-26 13:19:15 +0700

    update url

--- configs/app.ts
+++ configs/app.ts
@@ -1,3 +1,3 @@
-// export const API_URL = 'https://quiz-power-backend.onrender.com'
-export const API_URL = 'http://localhost:4001'
+export const API_URL = 'https://quiz-power-backend.onrender.com'
+// export const API_URL = 'http://localhost:4001'
 
